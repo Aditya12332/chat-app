72a962b23c576be1865639900f5765c8ddcfb16f
Aditya <adityasultania123321@gmail.com>
2025-05-13 22:53:32 +0530

    creating endpoints of login logout

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -52,10 +52,42 @@ export const signup = async (req, res) => {
     }
 };
 
-export const login = (req, res) => {
-    res.send("login route");
+export const login = async (req, res) => {
+    const {email, password} = req.body;
+    try {
+        const user = await User.findOne({email});
+
+        if(!user){
+            return res.status(400).json({message: "Invalid credentials"});
+        }
+     const isPasswordCorrect = await bcrypt.compare(password, user.password)
+        if(!isPasswordCorrect){
+            return res.status(400).json({message: "Invalid credentials"});
+        }
+        // Generate JWT token and send as a cookie
+        const token = generateToken(user._id, res);
+
+        // Send success response
+        return res.status(200).json({
+            message: "Login successful",
+            _id: user._id,
+            username: user.fullName,
+            email: user.email,
+            profilePic: user.profilePic,
+        });
+    } catch (error) {
+        console.log("Error in login controller", error.message);
+        return res.status(500).json({ message: "Internal server error" });
+    }
 };
 
 export const logout = (req, res) => {
-    res.send("logout route");
+  try {
+    
+    res.cookie("jwt", "", { maxAge:0});
+    return res.status(200).json({ message: "Logout successful" });
+  } catch (error) {
+    console.log("Error in logout controller", error.message);
+    return res.status(500).json({ message: "Internal server error" });
+  }
 };
