edb66b9cf3702e4ff46e5d53e79d4dc95977cea8
Aditya <adityasultania123321@gmail.com>
2026-02-15 14:04:58 +0530

    Removing Old frontend files and revamping new frontend with better configurations and newer models

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,25 +1,14 @@
 import React from 'react'
-import Navbar from './components/Navbar.jsx'
-import { Routes, Route } from 'react-router-dom'
-import HomePage from './pages/HomePage.jsx'
-import SignupPage from './pages/SignupPage.jsx'
-import LoginPage from './pages/LoginPage.jsx'
-import SettingsPage from './pages/SettingsPage.jsx'
-import ProfilePage from './pages/ProfilePage.jsx'
 
 const App = () => {
   return (
-    <div>
-      <Navbar />
-       <Routes>
-        <Route path='/' element= {<HomePage/>} />
-         <Route path='signup' element= {<SignupPage/>} />
-          <Route path='/login' element= {<LoginPage /> }/>
-           <Route path='/settings' element= {<SettingsPage/>} />
-            <Route path='/profile' element= {<ProfilePage/>} />
+    <>
+<button className="btn bg-slate-600 text-white">Default</button>
 
-    </Routes>
-    </div>
+</>
+
+
+
   )
 }
 
